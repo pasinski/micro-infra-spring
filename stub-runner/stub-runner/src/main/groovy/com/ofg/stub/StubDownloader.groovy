5bb412107416ed90986b33fc9c96e0d4a605055f
Marcin Grzejszczak <marcin@grzejszczak.pl>
2015-03-08 22:39:31 +0100

    [#245] WIP on trying to migrate from Grape to sth else aether - http://git.eclipse.org/c/aether/aether-demo.git/tree/aether-demo-snippets/src/main/java/org/eclipse/aether/examples/FindNewestVersion.java shrinkwrap - https://github.com/shrinkwrap/resolver

--- stub-runner/stub-runner/src/main/groovy/com/ofg/stub/StubDownloader.groovy
+++ stub-runner/stub-runner/src/main/groovy/com/ofg/stub/StubDownloader.groovy
@@ -2,12 +2,34 @@ package com.ofg.stub
 
 import com.ofg.stub.util.ZipCategory
 import groovy.util.logging.Slf4j
+import org.apache.maven.repository.internal.MavenRepositorySystemUtils
+import org.eclipse.aether.*
+import org.eclipse.aether.artifact.Artifact
+import org.eclipse.aether.artifact.DefaultArtifact
+import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory
+import org.eclipse.aether.impl.DefaultServiceLocator
+import org.eclipse.aether.repository.LocalRepository
+import org.eclipse.aether.repository.RemoteRepository
+import org.eclipse.aether.resolution.VersionRangeRequest
+import org.eclipse.aether.resolution.VersionRangeResult
+import org.eclipse.aether.spi.connector.RepositoryConnectorFactory
+import org.eclipse.aether.spi.connector.transport.TransporterFactory
+import org.eclipse.aether.transfer.AbstractTransferListener
+import org.eclipse.aether.transfer.MetadataNotFoundException
+import org.eclipse.aether.transfer.TransferEvent
+import org.eclipse.aether.transfer.TransferResource
+import org.eclipse.aether.transport.file.FileTransporterFactory
+import org.eclipse.aether.transport.http.HttpTransporterFactory
+import org.eclipse.aether.version.Version
+
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.util.concurrent.ConcurrentHashMap
 
 import static groovy.grape.Grape.addResolver
 import static groovy.grape.Grape.resolve
 import static groovy.io.FileType.FILES
 import static java.nio.file.Files.createTempDirectory
-
 
 /**
  * Downloads stubs from an external repository and unpacks them locally
@@ -53,11 +75,30 @@ class StubDownloader {
 
     private URI findGrabbedStubJars(boolean skipLocalRepo, String stubRepositoryRoot, String stubsGroup, String stubsModule) {
         Map depToGrab = [group: stubsGroup, module: stubsModule, version: LATEST_MODULE, transitive: false]
+
+
+        RepositorySystem system = Booter.newRepositorySystem();
+
+        RepositorySystemSession session = Booter.newRepositorySystemSession( system );
+
+        Artifact artifact = new DefaultArtifact( "$stubsGroup:$stubsModule:[,)" );
+
+        VersionRangeRequest rangeRequest = new VersionRangeRequest();
+        rangeRequest.setArtifact( artifact );
+        rangeRequest.setRepositories( Booter.newRepositories( system, session, stubRepositoryRoot ) );
+
+        VersionRangeResult rangeResult = system.resolveVersionRange( session, rangeRequest );
+
+        Version newestVersion = rangeResult.getHighestVersion();
+
+        System.out.println( "Newest version " + newestVersion + " from repository "
+                + rangeResult.getRepository( newestVersion ) );
+
         return buildResolver(skipLocalRepo).resolveDependency(stubRepositoryRoot, depToGrab)
     }
 
     private DependencyResolver buildResolver(boolean skipLocalRepo) {
-        skipLocalRepo ? new RemoteDependencyResolver() : new LocalFirstDependencyResolver()
+        return skipLocalRepo ? new RemoteDependencyResolver() : new LocalFirstDependencyResolver()
     }
 
     /**
@@ -146,4 +187,334 @@ class StubDownloader {
 
     }
 
+    class DependencyResolutionException extends RuntimeException {
+
+        DependencyResolutionException(String message, Throwable cause) {
+            super(message, cause)
+        }
+    }
+
+    /**
+     * A factory for repository system instances that employs Aether's built-in service locator infrastructure to wire up
+     * the system's components.
+     */
+    static class ManualRepositorySystemFactory
+    {
+
+        public static RepositorySystem newRepositorySystem()
+        {
+            /*
+             * Aether's components implement org.eclipse.aether.spi.locator.Service to ease manual wiring and using the
+             * prepopulated DefaultServiceLocator, we only need to register the repository connector and transporter
+             * factories.
+             */
+            DefaultServiceLocator locator = MavenRepositorySystemUtils.newServiceLocator();
+            locator.addService( RepositoryConnectorFactory.class, BasicRepositoryConnectorFactory.class );
+            locator.addService( TransporterFactory.class, FileTransporterFactory.class );
+            locator.addService( TransporterFactory.class, HttpTransporterFactory.class );
+
+            locator.setErrorHandler( new DefaultServiceLocator.ErrorHandler() {
+                @Override
+                public void serviceCreationFailed( Class<?> type, Class<?> impl, Throwable exception )
+                {
+                    exception.printStackTrace();
+                }
+            } );
+
+            return locator.getService( RepositorySystem.class );
+        }
+
+    }
+
+    /**
+     * A helper to boot the repository system and a repository system session.
+     */
+    static class Booter
+    {
+
+        public static RepositorySystem newRepositorySystem()
+        {
+            return ManualRepositorySystemFactory.newRepositorySystem();
+            // return org.eclipse.aether.examples.guice.GuiceRepositorySystemFactory.newRepositorySystem();
+            // return org.eclipse.aether.examples.sisu.SisuRepositorySystemFactory.newRepositorySystem();
+            // return org.eclipse.aether.examples.plexus.PlexusRepositorySystemFactory.newRepositorySystem();
+        }
+
+        public static DefaultRepositorySystemSession newRepositorySystemSession( RepositorySystem system )
+        {
+            DefaultRepositorySystemSession session = MavenRepositorySystemUtils.newSession();
+
+            LocalRepository localRepo = new LocalRepository( System.getProperty('MAVEN_HOME', "${System.getProperty('user.home')}/.m2") );
+            session.setLocalRepositoryManager( system.newLocalRepositoryManager( session, localRepo ) );
+
+            session.setTransferListener( new ConsoleTransferListener() );
+            session.setRepositoryListener( new ConsoleRepositoryListener() );
+
+            // uncomment to generate dirty trees
+            // session.setDependencyGraphTransformer( null );
+
+            return session;
+        }
+
+        public static List<RemoteRepository> newRepositories( RepositorySystem system, RepositorySystemSession session, String stubRepositoryRoot)
+        {
+            return new ArrayList<RemoteRepository>( Arrays.asList( newCentralRepository(stubRepositoryRoot) ) );
+        }
+
+        private static RemoteRepository newCentralRepository(String stubRepositoryRoot)
+        {
+            return new RemoteRepository.Builder( "nexus", "default", stubRepositoryRoot ).build();
+        }
+
+    }
+
+    static class ConsoleTransferListener
+            extends AbstractTransferListener
+    {
+
+        private PrintStream out;
+
+        private Map<TransferResource, Long> downloads = new ConcurrentHashMap<TransferResource, Long>();
+
+        private int lastLength;
+
+        public ConsoleTransferListener()
+        {
+            this( null );
+        }
+
+        public ConsoleTransferListener( PrintStream out )
+        {
+            this.out = ( out != null ) ? out : System.out;
+        }
+
+        @Override
+        public void transferInitiated( TransferEvent event )
+        {
+            String message = event.getRequestType() == TransferEvent.RequestType.PUT ? "Uploading" : "Downloading";
+
+            out.println( message + ": " + event.getResource().getRepositoryUrl() + event.getResource().getResourceName() );
+        }
+
+        @Override
+        public void transferProgressed( TransferEvent event )
+        {
+            TransferResource resource = event.getResource();
+            downloads.put( resource, Long.valueOf( event.getTransferredBytes() ) );
+
+            StringBuilder buffer = new StringBuilder( 64 );
+
+            for ( Map.Entry<TransferResource, Long> entry : downloads.entrySet() )
+            {
+                long total = entry.getKey().getContentLength();
+                long complete = entry.getValue().longValue();
+
+                buffer.append( getStatus( complete, total ) ).append( "  " );
+            }
+
+            int pad = lastLength - buffer.length();
+            lastLength = buffer.length();
+            pad( buffer, pad );
+            buffer.append( '\r' );
+
+            out.print( buffer );
+        }
+
+        private String getStatus( long complete, long total )
+        {
+            if ( total >= 1024 )
+            {
+                return toKB( complete ) + "/" + toKB( total ) + " KB ";
+            }
+            else if ( total >= 0 )
+            {
+                return complete + "/" + total + " B ";
+            }
+            else if ( complete >= 1024 )
+            {
+                return toKB( complete ) + " KB ";
+            }
+            else
+            {
+                return complete + " B ";
+            }
+        }
+
+        private void pad( StringBuilder buffer, int spaces )
+        {
+            String block = "                                        ";
+            while ( spaces > 0 )
+            {
+                int n = Math.min( spaces, block.length() );
+                buffer.append( block, 0, n );
+                spaces -= n;
+            }
+        }
+
+        @Override
+        public void transferSucceeded( TransferEvent event )
+        {
+            transferCompleted( event );
+
+            TransferResource resource = event.getResource();
+            long contentLength = event.getTransferredBytes();
+            if ( contentLength >= 0 )
+            {
+                String type = ( event.getRequestType() == TransferEvent.RequestType.PUT ? "Uploaded" : "Downloaded" );
+                String len = contentLength >= 1024 ? toKB( contentLength ) + " KB" : contentLength + " B";
+
+                String throughput = "";
+                long duration = System.currentTimeMillis() - resource.getTransferStartTime();
+                if ( duration > 0 )
+                {
+                    long bytes = contentLength - resource.getResumeOffset();
+                    DecimalFormat format = new DecimalFormat( "0.0", new DecimalFormatSymbols( Locale.ENGLISH ) );
+                    double kbPerSec = ( bytes / 1024.0 ) / ( duration / 1000.0 );
+                    throughput = " at " + format.format( kbPerSec ) + " KB/sec";
+                }
+
+                out.println( type + ": " + resource.getRepositoryUrl() + resource.getResourceName() + " (" + len
+                        + throughput + ")" );
+            }
+        }
+
+        @Override
+        public void transferFailed( TransferEvent event )
+        {
+            transferCompleted( event );
+
+            if ( !( event.getException() instanceof MetadataNotFoundException ) )
+            {
+                event.getException().printStackTrace( out );
+            }
+        }
+
+        private void transferCompleted( TransferEvent event )
+        {
+            downloads.remove( event.getResource() );
+
+            StringBuilder buffer = new StringBuilder( 64 );
+            pad( buffer, lastLength );
+            buffer.append( '\r' );
+            out.print( buffer );
+        }
+
+        public void transferCorrupted( TransferEvent event )
+        {
+            event.getException().printStackTrace( out );
+        }
+
+        protected long toKB( long bytes )
+        {
+            return ( bytes + 1023 ) / 1024;
+        }
+
+    }
+
+    /**
+     * A simplistic repository listener that logs events to the console.
+     */
+    static class ConsoleRepositoryListener
+            extends AbstractRepositoryListener
+    {
+
+        private PrintStream out;
+
+        public ConsoleRepositoryListener()
+        {
+            this( null );
+        }
+
+        public ConsoleRepositoryListener( PrintStream out )
+        {
+            this.out = ( out != null ) ? out : System.out;
+        }
+
+        public void artifactDeployed( RepositoryEvent event )
+        {
+            out.println( "Deployed " + event.getArtifact() + " to " + event.getRepository() );
+        }
+
+        public void artifactDeploying( RepositoryEvent event )
+        {
+            out.println( "Deploying " + event.getArtifact() + " to " + event.getRepository() );
+        }
+
+        public void artifactDescriptorInvalid( RepositoryEvent event )
+        {
+            out.println( "Invalid artifact descriptor for " + event.getArtifact() + ": "
+                    + event.getException().getMessage() );
+        }
+
+        public void artifactDescriptorMissing( RepositoryEvent event )
+        {
+            out.println( "Missing artifact descriptor for " + event.getArtifact() );
+        }
+
+        public void artifactInstalled( RepositoryEvent event )
+        {
+            out.println( "Installed " + event.getArtifact() + " to " + event.getFile() );
+        }
+
+        public void artifactInstalling( RepositoryEvent event )
+        {
+            out.println( "Installing " + event.getArtifact() + " to " + event.getFile() );
+        }
+
+        public void artifactResolved( RepositoryEvent event )
+        {
+            out.println( "Resolved artifact " + event.getArtifact() + " from " + event.getRepository() );
+        }
+
+        public void artifactDownloading( RepositoryEvent event )
+        {
+            out.println( "Downloading artifact " + event.getArtifact() + " from " + event.getRepository() );
+        }
+
+        public void artifactDownloaded( RepositoryEvent event )
+        {
+            out.println( "Downloaded artifact " + event.getArtifact() + " from " + event.getRepository() );
+        }
+
+        public void artifactResolving( RepositoryEvent event )
+        {
+            out.println( "Resolving artifact " + event.getArtifact() );
+        }
+
+        public void metadataDeployed( RepositoryEvent event )
+        {
+            out.println( "Deployed " + event.getMetadata() + " to " + event.getRepository() );
+        }
+
+        public void metadataDeploying( RepositoryEvent event )
+        {
+            out.println( "Deploying " + event.getMetadata() + " to " + event.getRepository() );
+        }
+
+        public void metadataInstalled( RepositoryEvent event )
+        {
+            out.println( "Installed " + event.getMetadata() + " to " + event.getFile() );
+        }
+
+        public void metadataInstalling( RepositoryEvent event )
+        {
+            out.println( "Installing " + event.getMetadata() + " to " + event.getFile() );
+        }
+
+        public void metadataInvalid( RepositoryEvent event )
+        {
+            out.println( "Invalid metadata " + event.getMetadata() );
+        }
+
+        public void metadataResolved( RepositoryEvent event )
+        {
+            out.println( "Resolved metadata " + event.getMetadata() + " from " + event.getRepository() );
+        }
+
+        public void metadataResolving( RepositoryEvent event )
+        {
+            out.println( "Resolving metadata " + event.getMetadata() + " from " + event.getRepository() );
+        }
+
+    }
 }
